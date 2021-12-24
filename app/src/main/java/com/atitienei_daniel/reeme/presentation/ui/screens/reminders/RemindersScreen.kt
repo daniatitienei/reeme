6876099037335fd00a6eb0678f9e79f7c001e9c9
daniatitienei <daniatitienei@gmail.com>
2021-12-24 20:07:31 +0200

    search bar ui done

--- app/src/main/java/com/atitienei_daniel/reeme/presentation/ui/screens/reminders/RemindersScreen.kt
+++ app/src/main/java/com/atitienei_daniel/reeme/presentation/ui/screens/reminders/RemindersScreen.kt
@@ -1,20 +1,32 @@
 package com.atitienei_daniel.reeme.presentation.ui.screens.reminders
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.*
+import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.atitienei_daniel.reeme.presentation.theme.DarkBlue700
@@ -30,7 +42,7 @@ import kotlinx.coroutines.launch
 fun RemindersScreen() {
 
     val modalBottomSheetState =
-        rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.HalfExpanded)
+        rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
     val scope = rememberCoroutineScope()
 
     val checkedList = remember {
@@ -44,6 +56,22 @@ fun RemindersScreen() {
     var newCategoryTitle by remember {
         mutableStateOf("")
     }
+
+    var searchBarValue by remember {
+        mutableStateOf("")
+    }
+
+    val interactionSource = MutableInteractionSource()
+
+    val isFocused by interactionSource.collectIsFocusedAsState()
+
+    val searchIconColor by animateColorAsState(
+        targetValue = if (isFocused && searchBarValue.isNotEmpty()) DarkBlue800 else DarkBlue800.copy(
+            alpha = 0.5f
+        ), animationSpec = tween(300)
+    )
+
+    val backdropState = rememberBackdropScaffoldState(initialValue = BackdropValue.Concealed)
 
     if (isDialogOpened)
         AlertDialog(
@@ -154,79 +182,146 @@ fun RemindersScreen() {
             }
         }
     ) {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = { Text(text = "Reminders") },
-                    navigationIcon = {
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(Icons.Rounded.Menu, contentDescription = null)
-                        }
-                    },
-                    actions = {
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(Icons.Rounded.Search, contentDescription = null)
-                        }
+        BackdropScaffold(
+            scaffoldState = backdropState,
+            appBar = {
+                Crossfade(targetState = backdropState.isConcealed) { isConcealed ->
+                    if (isConcealed)
+                        TopAppBar(
+                            title = { Text(text = "Reminders") },
+                            navigationIcon = {
+                                IconButton(onClick = { /*TODO*/ }) {
+                                    Icon(Icons.Rounded.Menu, contentDescription = null)
+                                }
+                            },
+                            actions = {
+                                IconButton(onClick = {
+                                    scope.launch {
+                                        backdropState.reveal()
+                                    }
+                                }) {
+                                    Icon(Icons.Rounded.Search, contentDescription = null)
+                                }
 
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(Icons.Rounded.FilterList, contentDescription = null)
-                        }
-                    },
-                    backgroundColor = MaterialTheme.colors.background,
-                    contentColor = MaterialTheme.colors.primary
-                )
-            },
-            floatingActionButton = {
-                FloatingActionButton(onClick = {
-                    scope.launch {
-                        if (modalBottomSheetState.isVisible) {
-                            modalBottomSheetState.hide()
-                        } else {
-                            modalBottomSheetState.show()
-                        }
-                    }
-                }) {
-                    Icon(Icons.Rounded.Add, contentDescription = null)
-                }
-            }
-        ) { innerPadding ->
+                                IconButton(onClick = { /*TODO*/ }) {
+                                    Icon(Icons.Rounded.FilterList, contentDescription = null)
+                                }
+                            },
+                            backgroundColor = MaterialTheme.colors.background,
+                            contentColor = MaterialTheme.colors.primary
+                        )
+                    else
+                        TopAppBar(
+                            title = {
+                                BasicTextField(
+                                    value = searchBarValue,
+                                    onValueChange = { searchBarValue = it },
+                                    textStyle = TextStyle(
+                                        fontSize = MaterialTheme.typography.body2.fontSize,
+                                        fontFamily = MaterialTheme.typography.body2.fontFamily,
+                                        color = MaterialTheme.typography.body2.color
+                                    ),
+                                    singleLine = true,
+                                    keyboardOptions = KeyboardOptions(
+                                        imeAction = ImeAction.Search
+                                    ),
+                                    interactionSource = interactionSource,
+                                    decorationBox = { innerTextField ->
+                                        Row(
+                                            modifier = Modifier
+                                                .clip(CircleShape)
+                                                .fillMaxWidth(0.9f)
+                                                .background(Color.Black.copy(alpha = 0.07f))
+                                                .padding(horizontal = 10.dp, vertical = 5.dp),
+                                            verticalAlignment = Alignment.CenterVertically
+                                        ) {
+                                            Icon(
+                                                Icons.Rounded.Search,
+                                                contentDescription = null,
+                                                tint = searchIconColor
+                                            )
 
-            LazyColumn(
-                contentPadding = PaddingValues(horizontal = 15.dp, vertical = 15.dp)
-            ) {
-                item {
-                    Text(text = "Upcoming")
+                                            Spacer(modifier = Modifier.width(16.dp))
 
-                    Spacer(modifier = Modifier.height(10.dp))
-
-                    StaggeredVerticalGrid {
-                        repeat(18) {
-                            Box(
-                                modifier = Modifier.padding(
-                                    start = if (it % 2 != 0) 5.dp else 0.dp,
-                                    end = if (it % 2 != 0) 0.dp else 5.dp,
-                                    bottom = 10.dp
+                                            Box {
+                                                Crossfade(targetState = searchBarValue.isEmpty()) { isEmpty ->
+                                                    if (isEmpty)
+                                                        Text(
+                                                            text = "Search something",
+                                                            style = MaterialTheme.typography.body2,
+                                                            color = MaterialTheme.typography.body2.color.copy(
+                                                                alpha = 0.5f
+                                                            )
+                                                        )
+                                                }
+                                                innerTextField()
+                                            }
+                                        }
+                                    }
                                 )
-                            ) {
-                                if (it % 3 != 0)
-                                    ReminderCard(
-                                        color = Color(0xffD2F49B),
-                                        title = "Take a pill",
-                                        description = "Lorem ipsum lorem ipsum lorem ipsumLorem ipsum lorem ipsum lorem ipsumLorem ipsum lorem ipsum lorem ipsumLorem ipsum lorem ipsum lorem ipsum",
-                                        time = "Tommorow, 4:20"
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = {
+                                    scope.launch {
+                                        backdropState.conceal()
+                                    }
+                                }) {
+                                    Icon(Icons.Rounded.Close, contentDescription = null)
+                                }
+                            },
+                            backgroundColor = MaterialTheme.colors.background,
+                            contentColor = MaterialTheme.colors.primary
+                        )
+                }
+            },
+            backLayerContent = {
+                Column(modifier = Modifier.fillMaxSize()) {
+
+                }
+            },
+            headerHeight = 0.dp,
+            stickyFrontLayer = false,
+            gesturesEnabled = false,
+            backLayerBackgroundColor = MaterialTheme.colors.background,
+            frontLayerContent = {
+                LazyColumn(
+                    contentPadding = PaddingValues(horizontal = 15.dp, vertical = 15.dp)
+                ) {
+                    item {
+                        Text(text = "Upcoming")
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        StaggeredVerticalGrid {
+                            repeat(18) {
+                                Box(
+                                    modifier = Modifier.padding(
+                                        start = if (it % 2 != 0) 5.dp else 0.dp,
+                                        end = if (it % 2 != 0) 0.dp else 5.dp,
+                                        bottom = 10.dp
                                     )
-                                else
-                                    ReminderCard(
-                                        color = Color(0xffF49B9B),
-                                        title = "Take a pill",
-                                        description = "Lorem ipsum lorem ipsum lorem ipsum",
-                                        time = "Tommorow, 4:20"
-                                    )
+                                ) {
+                                    if (it % 3 != 0)
+                                        ReminderCard(
+                                            color = Color(0xffD2F49B),
+                                            title = "Take a pill",
+                                            description = "Lorem ipsum lorem ipsum lorem ipsumLorem ipsum lorem ipsum lorem ipsumLorem ipsum lorem ipsum lorem ipsumLorem ipsum lorem ipsum lorem ipsum",
+                                            time = "Tommorow, 4:20"
+                                        )
+                                    else
+                                        ReminderCard(
+                                            color = Color(0xffF49B9B),
+                                            title = "Take a pill",
+                                            description = "Lorem ipsum lorem ipsum lorem ipsum",
+                                            time = "Tommorow, 4:20"
+                                        )
+                                }
                             }
                         }
                     }
                 }
             }
+        ) {
         }
     }
 }
